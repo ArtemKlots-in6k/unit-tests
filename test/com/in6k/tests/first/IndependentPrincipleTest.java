c6e7d81efa5ae496b3161db6c93bfd2cc146a846
Artem Klots <ArtemKl@interlink-ua.com>
2016-07-06 15:21:24 +0300

    Fixed all tests

--- test/com/in6k/tests/first/IndependentPrincipleTest.java
+++ test/com/in6k/tests/first/IndependentPrincipleTest.java
@@ -3,12 +3,18 @@ package com.in6k.tests.first;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class IndependentPrincipleTest {
 
-    public static int result = 0;
+    public int result = 0;
+
+    @Before
+    public void setup(){
+        System.out.println(result);
+    }
 
     @Test
     public void test2() throws Exception {
@@ -17,7 +23,6 @@ public class IndependentPrincipleTest {
     }
 
     @Test
-    @Ignore
     public void test1() throws Exception {
         result += 2;
         assertEquals(2, result);
